52473219d35828da3e9f7cd43cb1db0605305271
Stephen Chao <stephen.liu.chao@protonmail.com>
2023-09-11 12:00:03 -0700

    finished initialize

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -1,17 +1,26 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board=board
+    @next_mover_mark=next_mover_mark
+    @prev_move_pos=prev_move_pos
   end
+  
 
   def losing_node?(evaluator)
+
   end
 
   def winning_node?(evaluator)
+
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    
   end
 end
+
