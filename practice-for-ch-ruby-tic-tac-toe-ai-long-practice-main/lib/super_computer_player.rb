db34675b18627a6ac9ca3bebe9cb08839fb2aafc
yuris <yls4@cornell.edu>
2023-09-11 20:21:52 -0400

    finished game

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/super_computer_player.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/super_computer_player.rb
@@ -2,6 +2,20 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    not_lose = []
+    node.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos 
+      elsif !child.losing_node?(mark)
+        not_lose << child.prev_move_pos 
+      end
+    end
+    if not_lose.length == 0
+      raise "error"
+    else
+      return not_lose[0]
+    end
   end
 end
 
